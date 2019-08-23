42b2ed6580c7d137ac3934b4a33cac8b74ebbbc8
2017200442 <2017200442@UCMVEN042.UC.ACAD>
2019-08-23 20:33:37 -0300

    coisas que de nada servem, somente para aprendizado

--- Aulas POO/src/ExercicioAula22do08.java
+++ Aulas POO/src/ExercicioAula22do08.java
@@ -2,8 +2,19 @@
 public class ExercicioAula22do08 {
 
 	public static void main(String[] args) {
-		//teste
-
+		double soma = calculaAlgo(2,4);
+		System.out.println(soma);
+	}
+	
+	/**
+	 * {@value Função calcula quase nada rs}
+	 * @param valor1
+	 * @param valor2
+	 * @return
+	 */
+	public static double calculaAlgo(double valor1, double valor2) {
+		double x = valor1 + valor2;
+		return x;
 	}
 
 }
