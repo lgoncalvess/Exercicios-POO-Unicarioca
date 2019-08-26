71a85bd638b384b44205dda895a5d2c539bde287
Leonardo Gonçalves <leogonsil97@gmail.com>
2019-08-26 13:23:28 -0300

    Exercicio hum e dois seleção

---
+++ Aulas POO/src/ExercicioDoisSelecao.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ExercicioDoisSelecao {
+
+	public static void main(String[] args) {
+		double n1,n2,n3,media;
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite a nota 1: ");
+		n1 = entrada.nextDouble();
+		System.out.println("Digite a nota 2: ");
+		n2 = entrada.nextDouble();
+		System.out.println("Digite a nota 3 (CASO NÃO TENHA FEITO DIGITE -1) : ");
+		n3 = entrada.nextDouble();
+		if(n3 == -1) {
+			media = (n1 + n2) / 2;
+		}else {
+			if(n3 > n1) {
+				media = (n3 + n2) / 2;
+			}else if(n3 > n2) {
+				media = (n1 + n3) / 2;
+			}else {
+				media = (n1 + n2) / 2;
+			}
+		}
+		if(media >= 6) System.out.println("APROVADO!");
+		if(media < 3) System.out.println("REPROVADO!");
+		if(media >= 3 && media < 6) System.out.println("EXAME!");
+		entrada.close();
+	}
+
+}
---
+++ Aulas POO/src/ExercicioHumSeleção.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class ExercicioHumSeleção {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int cod;
+		System.out.println("Digite o código de origem do produto: ");
+		cod = entrada.nextInt();
+		switch(cod){
+		case 1:
+			System.out.println("SUL");
+			break;
+		case 2:
+			System.out.println("NORTE");
+			break;
+		case 3:
+			System.out.println("LESTE");
+			break;
+		case 4:
+			System.out.println("OESTE");
+			break;
+		case 5:
+			System.out.println("NORDESTE");
+			break;
+		case 6:
+			System.out.println("NORDESTE");
+			break;
+		case 7:
+			System.out.println("SUDESTE");
+			break;
+		case 8:
+			System.out.println("SUDESTE");
+			break;
+		case 9:
+			System.out.println("SUDESTE");
+			break;
+		case 10:
+			System.out.println("CENTRO-OESTE");
+			break;
+		case 11:
+			System.out.println("NOROESTE");
+			break;
+		default:
+			System.out.println("Importado");
+			break;
+		}
+		entrada.close();
+	}
+
+}
