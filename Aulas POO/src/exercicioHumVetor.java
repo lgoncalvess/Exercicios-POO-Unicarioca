e9155900f0ca643248c1873183dd85a1e154e643
Leonardo Gonçalves <leogonsil97@gmail.com>
2019-09-02 12:51:12 -0300

    Exercicio 1 vetores

---
+++ Aulas POO/src/exercicioHumVetor.java
@@ -0,0 +1,16 @@
+
+public class exercicioHumVetor {
+
+	public static void main(String[] args) {
+		int vetX[] = new int[10];
+		for(int num = 0;num <vetX.length;num++) {
+			vetX[num] = 30;
+		}
+		
+		for(int i = 0;i < vetX.length;i++) {
+			System.out.println(vetX[i]);
+		}
+
+	}
+
+}
