539105632000af472ab310ec9a3be4191696acc3
Leonardo Gonçalves <leogonsil97@gmail.com>
2019-09-02 13:00:10 -0300

    exercicio dois Vetor

---
+++ Aulas POO/src/exercicioDoisVetor.java
@@ -0,0 +1,16 @@
+
+public class exercicioDoisVetor {
+
+	public static void main(String[] args) {
+		int vetA[] = new int[10];
+		for(int num = 0;num <vetA.length;num++) {
+			vetA[num] = num + 1;
+		}
+		
+		for(int i = 0;i < vetA.length;i++) {
+			System.out.println(vetA[i]); 
+		}
+
+	}
+
+}
